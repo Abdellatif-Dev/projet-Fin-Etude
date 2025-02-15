880b887664635b283038c693d36cf12dc8bae430
abdellatif <abdellatif.aboumadane@gmail.com>
2025-02-15 23:22:50 +0100

    projet v1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppManga from './ProjetManga/AppManga';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
